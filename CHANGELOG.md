73346d6a2f8aaa60b95037c68cf6387b60100aaf
Scott Motte <mot@mot.la>
2022-10-28 23:10:18 -0700

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
-## [Unreleased](https://github.com/dotenv-org/dotenv-vault-python/compare/v0.0.9...master)
+## [Unreleased](https://github.com/dotenv-org/python-dotenv-vault/compare/v0.0.9...master)
 
 ## 0.0.9
 
--- src/dotenv_vault/__version__.py
+++ src/dotenv_vault/__version__.py
@@ -1,13 +1,7 @@
-#      _     _                                 _ _                 _   _             
-#   __| |___| |_ ___ _ ___ _______ ____ _ _  _| | |_ ___ _ __ _  _| |_| |_  ___ _ _  
-#  / _` / _ \  _/ -_) ' \ V /___\ V / _` | || | |  _|___| '_ \ || |  _| ' \/ _ \ ' \ 
-#  \__,_\___/\__\___|_||_\_/     \_/\__,_|\_,_|_|\__|   | .__/\_, |\__|_||_\___/_||_|
-#                                                       |_|   |__/                   
-
 __title__ = "python-dotenv-vault"
-__description__ = "Syncing your .env to the cloud."
-__url__ = "https://github.com/dotenv-org/dotenv-vault-python"
-__version__ = "0.0.9"
+__description__ = "Decrypt .env.vault file."
+__url__ = "https://github.com/dotenv-org/python-dotenv-vault"
+__version__ = "0.1.0"
 __author__ = "dotenv"
 __author_email__ = "mot@dotenv.org"
 __license__ = "MIT"
