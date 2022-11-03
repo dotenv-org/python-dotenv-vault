dfab706182061b1f12647ff68da51f01e6d7c382
Nguyen Nguyen <nsnguyen@users.noreply.github.com>
2022-11-02 20:23:51 -0700

    add key rotation logic

--- src/dotenv_vault/vault.py
+++ src/dotenv_vault/vault.py
@@ -1,14 +1,14 @@
 
 import logging
 import os
-import sys
 import io
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.exceptions import InvalidTag
 from base64 import b64decode
 from urllib.parse import urlparse, parse_qsl
 
-from dotenv.main import DotEnv, find_dotenv, load_dotenv
+from dotenv.main import DotEnv, find_dotenv
 
 
 logging.basicConfig(level = logging.INFO)
@@ -31,37 +31,45 @@ class DotEnvVault(): #vault stuff
         Parse information from DOTENV_KEY, and decrypt vault key.
         """
         if self.dotenv_key is None: raise DotEnvVaultError("NOT_FOUND_DOTENV_KEY: Cannot find ENV['DOTENV_KEY']")
-            
+
         # .env.vault needs to be present.
         env_vault_path = find_dotenv(filename='.env.vault', usecwd=True)
+        keys = []
         if env_vault_path == '':
             raise DotEnvVaultError("ENV_VAULT_NOT_FOUND: .env.vault is not present.")
 
-        # parse DOTENV_KEY, format is a URI
-        uri = urlparse(self.dotenv_key)
-        # Get encrypted key
-        key = uri.password
-        # Get environment from query params.
-        params = dict(parse_qsl(uri.query))
-        vault_environment = params.get('environment').upper()
+        dotenv_keys = [i.strip() for i in self.dotenv_key.split(',')]
+        for _key in dotenv_keys:    
+            # parse DOTENV_KEY, format is a URI
+            uri = urlparse(_key)
+            # Get encrypted key
+            key = uri.password
+            # Get environment from query params.
+            params = dict(parse_qsl(uri.query))
+            vault_environment = params.get('environment').upper()
 
-        if vault_environment is None or vault_environment not in ['PRODUCTION', 'DEVELOPMENT', 'CI', 'STAGING']:
-            raise DotEnvVaultError('Incorrect Vault Environment.')
+            if vault_environment is None or vault_environment not in ['PRODUCTION', 'DEVELOPMENT', 'CI', 'STAGING']:
+                raise DotEnvVaultError('Incorrect Vault Environment. It is: {vault_environment}')
 
-        # Getting ciphertext from correct environment in .env.vault
-        environment_key = f'DOTENV_VAULT_{vault_environment}'
-        logging.info(f'Getting key from {environment_key}.')
+            # Getting ciphertext from correct environment in .env.vault
+            environment_key = f'DOTENV_VAULT_{vault_environment}'
+            logging.info(f'Getting key from {environment_key}.')
 
-        # use python-dotenv library class.
-        dotenv = DotEnv(dotenv_path=env_vault_path)
-        ciphertext = dotenv.dict().get(environment_key)
+            # use python-dotenv library class.
+            dotenv = DotEnv(dotenv_path=env_vault_path)
+            ciphertext = dotenv.dict().get(environment_key)
 
-        if not ciphertext:
-            raise DotEnvVaultError('Environment Key is not found. Run `npx dotenv-vault build`.')
+            if not ciphertext:
+                raise DotEnvVaultError('Environment Key is not found. Run `npx dotenv-vault build`.')
 
-        decrypted = self._decrypt(ciphertext=ciphertext, key=key)
+            keys.append({
+                'encrypted_key': key,
+                'ciphertext': ciphertext
+            })
+
+        decrypted = self._key_rotation(keys=keys)
         return self._to_text_stream(decrypted)
-
+            
 
     def _decrypt(self, ciphertext: str, key: str) -> bytes:
         """
@@ -69,7 +77,8 @@ class DotEnvVault(): #vault stuff
         return: decrypted keys in bytes
         """
         _key = key[4:]
-        if len(_key) < 64: raise DotEnvVault('INVALID_DOTENV_KEY: Key part must be 64 characters long (or more)')
+        if len(_key) < 64: 
+            raise DotEnvVault('INVALID_DOTENV_KEY: Key part must be 64 characters long (or more)')
 
         _key = bytes.fromhex(_key)
         ciphertext = b64decode(ciphertext)
@@ -85,3 +94,17 @@ class DotEnvVault(): #vault stuff
         """
         decoded_str = decrypted_obj.decode('utf-8')
         return io.StringIO(decoded_str)
+
+    def _key_rotation(self, keys: list[dict]) -> str:
+        """
+        Iterate through list of keys to check for correct one.
+        """
+        _len = len(keys)
+        for i, k in enumerate(keys):
+            try:
+                return self._decrypt(ciphertext=k['ciphertext'], key=k['encrypted_key'])
+            except InvalidTag:
+                if i + 1 >= _len: # exhaust all keys
+                    raise DotEnvVaultError('INVALID_DOTENV_KEY: Key must be valid.')
+                else:
+                    continue
