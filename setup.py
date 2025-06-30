58d86d6d0fb810a318f2a060c5c38171848796b8
Rob Aleck <mnbf9rca@users.noreply.github.com>
2025-06-30 19:17:05 +0000

    losen version requirements for lower python versions;

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     'dotenv-vault'
     ],
     install_requires=[
-        'python-dotenv~=1.1.1',
-        'cryptography>=45.0.0'
+        'python-dotenv>=0.21.0',
+        'cryptography>=41.0.0'
     ],
 )
